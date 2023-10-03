9d6abf2d5952970f422ee8f281cfc8cde8972537
Melozzo <carmecamma1995@gmail.com>
2023-10-03 10:54:32 +0200

    [edit]: contactcurrent tu active selected chat

--- js/script.js
+++ js/script.js
@@ -25,17 +25,17 @@ createApp ({
                     status: 'received'
                 }
             ],
-        },
-        {
+          },
+          {
             name: 'Fabio',
             avatar: './img/avatar_2.jpg',
             visible: true,
             messages: [
-                {
-                    date: '20/03/2020 16:30:00',
+              {
+                date: '20/03/2020 16:30:00',
                     message: 'Ciao come stai?',
                     status: 'sent'
-                },
+                  },
                 {
                     date: '20/03/2020 16:30:55',
                     message: 'Bene grazie! Stasera ci vediamo?',
@@ -49,33 +49,33 @@ createApp ({
             ],
         },
         {
-            name: 'Samuele',
-            avatar: './img/avatar_3.jpg',
+          name: 'Samuele',
+          avatar: './img/avatar_3.jpg',
             visible: true,
             messages: [
-                {
+              {
                     date: '28/03/2020 10:10:40',
                     message: 'La Marianna va in campagna',
                     status: 'received'
-                },
+                  },
                 {
                     date: '28/03/2020 10:20:10',
                     message: 'Sicuro di non aver sbagliato chat?',
                     status: 'sent'
-                },
+                  },
                 {
-                    date: '28/03/2020 16:15:22',
-                    message: 'Ah scusa!',
-                    status: 'received'
+                  date: '28/03/2020 16:15:22',
+                  message: 'Ah scusa!',
+                  status: 'received'
                 }
             ],
         },
         {
-            name: 'Alessandro B.',
-            avatar: './img/avatar_4.jpg',
-            visible: true,
-            messages: [
-                {
+          name: 'Alessandro B.',
+          avatar: './img/avatar_4.jpg',
+          visible: true,
+          messages: [
+            {
                     date: '10/01/2020 15:30:55',
                     message: 'Lo sai che ha aperto una nuova pizzeria?',
                     status: 'sent'
@@ -88,22 +88,22 @@ createApp ({
             ],
         },
         {
-            name: 'Alessandro L.',
-            avatar: './img/avatar_5.jpg',
-            visible: true,
-            messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    message: 'Ricordati di chiamare la nonna',
+          name: 'Alessandro L.',
+          avatar: './img/avatar_5.jpg',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              message: 'Ricordati di chiamare la nonna',
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                  date: '10/01/2020 15:50:00',
                     message: 'Va bene, stasera la sento',
                     status: 'received'
-                }
-            ],
-        },
+                  }
+                ],
+              },
         {
             name: 'Claudia',
             avatar: './img/avatar_6.jpg',
@@ -113,44 +113,44 @@ createApp ({
                     date: '10/01/2020 15:30:55',
                     message: 'Ciao Claudia, hai novità?',
                     status: 'sent'
-                },
+                  },
                 {
                     date: '10/01/2020 15:50:00',
                     message: 'Non ancora',
                     status: 'received'
-                },
+                  },
                 {
-                    date: '10/01/2020 15:51:00',
-                    message: 'Nessuna nuova, buona nuova',
-                    status: 'sent'
+                  date: '10/01/2020 15:51:00',
+                  message: 'Nessuna nuova, buona nuova',
+                  status: 'sent'
                 }
-            ],
+              ],
         },
         {
-            name: 'Federico',
-            avatar: './img/avatar_7.jpg',
-            visible: true,
-            messages: [
+          name: 'Federico',
+          avatar: './img/avatar_7.jpg',
+          visible: true,
+          messages: [
                 {
                     date: '10/01/2020 15:30:55',
                     message: 'Fai gli auguri a Martina che è il suo compleanno!',
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                  date: '10/01/2020 15:50:00',
                     message: 'Grazie per avermelo ricordato, le scrivo subito!',
                     status: 'received'
                 }
-            ],
+              ],
         },
         {
             name: 'Davide',
             avatar: './img/avatar_8.jpg',
             visible: true,
             messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    message: 'Ciao, andiamo a mangiare la pizza stasera?',
+              {
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao, andiamo a mangiare la pizza stasera?',
                     status: 'received'
                 },
                 {
@@ -164,9 +164,18 @@ createApp ({
                     status: 'received'
                 }
             ]
-        }
-      ]
-    }
+          }
+        ],
+        contactCurrent:0
+    }  
+  },
+
+  methods: {
+    // funzione per impostare l'index del contatto cliccato
+    setIndexContact(position) {
+        this.contactCurrent = position;
+        return this.contactCurrent;
+    },
   }
   
 
